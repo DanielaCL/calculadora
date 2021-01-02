dc41f8c466aebb975ec44932f0abfddb3d4a9152
Daniela <73953625+DanielaCL@users.noreply.github.com>
2021-01-02 14:54:38 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,28 @@
+let calculadora = {
+    sumar : function (numA, numB){
+        return numA + numB
+    },
+    restar: function (numA,numB){
+        return numA - numB 
+    },
+    multiplicar: function (numA, numB){
+        return numA * numB
+    },
+    dividir: function(numA, numB){
+      if (numA !== 0 && numB !== 0){
+        return numA /numB 
+    }else{
+        let mensaje = 'Error'
+        return mensaje 
+    }
+          
+    }
+
+}
+console.log (calculadora.sumar(2,2))
+
+console.log (calculadora.restar(5,3))
+
+console.log (calculadora.multiplicar(2,3))
+
+console.log (calculadora.dividir(2,2))
